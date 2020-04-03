c7f7ba1aba10909d7a7254ed1e95232b91178449
Vladimir Panteleev <git@thecybershadow.net>
2020-04-03 09:09:03 +0000

    monocre.output.html: Format to sink (reduce allocations)

--- output/html.d
+++ output/html.d
@@ -2,15 +2,16 @@ module monocre.output.html;
 
 import std.format;
 
+import monocre.output;
 import monocre.charimage;
 
-void outputHTML(in ref CharImage i, void delegate(string) sink)
+void outputHTML(in ref CharImage i, Sink sink)
 {
 	sink(`<html>`);
 	foreach (layer; i.layers)
 	{
-		sink(`<pre style="position: absolute; top: %dpx; left: %dpx">`
-			.format(layer.x0, layer.y0));
+		sink.formattedWrite!`<pre style="position: absolute; top: %dpx; left: %dpx">`
+			(layer.x0, layer.y0);
 		foreach (y, row; layer.chars)
 		{
 			CharImage.Char last;
@@ -19,12 +20,11 @@ void outputHTML(in ref CharImage i, void delegate(string) sink)
 				if (last && (last.bg != c.bg || last.fg != c.fg))
 					sink(`</span>`);
 				if (c && (last.bg != c.bg || last.fg != c.fg))
-					sink(`<span style="background-color: rgba(%d,%d,%d,%f); color: rgba(%d,%d,%d,%f);">`
-						.format(
-							c.bg.r, c.bg.g, c.bg.b, c.bg.a / 255.,
-							c.fg.r, c.fg.g, c.fg.b, c.fg.a / 255.,
-						));
-				sink("&#x%x;".format(uint(c ? c.c : ' ')));
+					sink.formattedWrite!`<span style="background-color: rgba(%d,%d,%d,%f); color: rgba(%d,%d,%d,%f);">`(
+						c.bg.r, c.bg.g, c.bg.b, c.bg.a / 255.,
+						c.fg.r, c.fg.g, c.fg.b, c.fg.a / 255.,
+					);
+				sink.formattedWrite!"&#x%x;"(uint(c ? c.c : ' '));
 				last = c;
 			}
 			if (last)
