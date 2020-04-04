0266fd805d8dfb852e3c6a2c487be2d20acbcd28
Vladimir Panteleev <git@thecybershadow.net>
2020-04-04 13:34:28 +0000

    monocre: Add license header

--- src/monocre/learn.d
+++ src/monocre/learn.d
@@ -1,3 +1,16 @@
+/**
+ * Implements the learn operation.
+ *
+ * License:
+ *   This Source Code Form is subject to the terms of
+ *   the Mozilla Public License, v. 2.0. If a copy of
+ *   the MPL was not distributed with this file, You
+ *   can obtain one at http://mozilla.org/MPL/2.0/.
+ *
+ * Authors:
+ *   Vladimir Panteleev <vladimir@thecybershadow.net>
+ */
+
 module monocre.learn;
 
 import core.internal.utf;
